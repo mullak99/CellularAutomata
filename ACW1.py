c34e3ca4342399a0d6ec5cdd58372bb6a537762f
mullak99 <mullak99@mullak99.co.uk>
2021-03-12 16:36:02 +0000

    Reverted some stuff since it wasnt working

--- ACW1.py
+++ ACW1.py
@@ -1,6 +1,7 @@
 import GridUtils as utils
 import SpreadOfFire as sim
 
-simulationGrids = sim.runSimulation(0.75, 0.2, 0.2, 0.1, 10, 8)
+nGrid = sim.createForestGrid(0.75, 0.2, 10)
+gridMultiStep = sim.runSimulation(nGrid, 0.2, 0.1, 8)
 
-utils.printGrids(simulationGrids, "Fire Spread Simulation")  # Simulation Grids
+utils.printGrids(gridMultiStep, "Simulation")
