967843fa24ce2f3cae5fa031811c5f5bc4ec1b7f
mullak99 <mullak99@mullak99.co.uk>
2021-03-12 16:10:51 +0000

    Removed redundant test code

--- ACW1.py
+++ ACW1.py
@@ -4,11 +4,9 @@ import SpreadOfFire as sim
 nGrid = sim.createForestGrid(0.75, 0.2, 10)
 gridEx = sim.extendGridBoundary(nGrid)
 gridSingleStep = sim.runSimulationOnce(nGrid, 0.2, 0.1)
-gridSingleStepRemove = sim.removeGridBoundary(gridSingleStep)
 gridMultiStep = sim.runSimulation(nGrid, 0.2, 0.1, 8)
 
 utils.printGrid(nGrid, "Grid")  # Normal 10x10 Grid
 utils.printGrid(gridEx, "GridEx")  # w/ boundary (12x12 with Normal 10x10 Grid inside)
 utils.printGrid(gridSingleStep, "GridSingleStep")  # nGrid w/ a single timestep
-utils.printGrid(gridSingleStepRemove, "GridSingleStepRemove")  # gridSingleStep w/ boundary removed
 utils.printGrids(gridMultiStep, "GridMultiStep")  # nGrid w/ a multi timesteps
