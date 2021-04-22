6d655e06914ef4f3a8145d780dd1b26f14747063
mullak99 <mullak99@mullak99.co.uk>
2021-04-22 12:21:31 +0100

    Now working code, implemented both sequential (single-threadded) and parallelised (multi-threadded) code, CPU stats are shown, simulation stats are also shown

--- ACW1.py
+++ ACW1.py
@@ -1,7 +1,41 @@
-import GridUtils as utils
-import SpreadOfFire as sim
+import SpreadOfFire as simSeq
+import SpreadOfFireJit as simPar
+import multiprocessing as mp
+import cpuinfo
+import time
 
-nGrid = sim.createForestGrid(0.75, 0.2, 10)
-gridMultiStep = sim.runSimulation(nGrid, 0.2, 0.1, 8)
 
-utils.printGrids(gridMultiStep, "Simulation")
+if __name__ == '__main__':
+    iterations = 5
+    gridSize = 800
+    probTree = 0.8
+    probBurning = 0.01
+    probImmune = 0.3
+    probLighting = 0.001
+
+    print("= CPU Stats =")
+    print("CPU:", cpuinfo.get_cpu_info()["brand_raw"])
+    print("CPU Arch:", cpuinfo.get_cpu_info()["arch_string_raw"])
+    print("CPU threads:", mp.cpu_count())
+
+    print("\n= Sim Stats =")
+    print("Iterations:", iterations)
+    print("Grid Size:", gridSize)
+    print("Tree Probability:", probTree)
+    print("Burning Probability:", probBurning)
+    print("Immune Probability:", probImmune)
+    print("Lightning Probability:", probLighting)
+
+    # Row Normalisation of a 2D array
+    fGrid = simSeq.forestGrid(probTree, probBurning, gridSize)
+
+    print("\n= Performance Stats =")
+    # No parallelisation
+    startTime = time.time()
+    simSeq.Simulation(iterations, fGrid, probImmune, probLighting)
+    print("--- %s seconds - No Parallelisation---" % (time.time() - startTime))
+
+    # parallelisation - Numba
+    startTime = time.time()
+    simPar.Simulation(iterations, fGrid, probImmune, probLighting)
+    print("--- %s seconds - Parallelisation with Numba---" % (time.time() - startTime))
--- SpreadOfFireJit.py
+++ SpreadOfFireJit.py
@@ -1,28 +1,14 @@
+import numba
 import numpy as np
 import random
 import cv2
 
-def forestGrid(probTree, probBurning, size):  # Prob Tree = 0.8 , Prob Burning = 0.01
-    grid = np.zeros((size, size))
-    for i in range(size):
-        for j in range(size):
-            isBurn = random.random()
-            isTree = random.random()
-            if isTree < probTree:  # Been Switched from greater than
-                if isBurn < probBurning:  # Been Switched from less than
-                    grid[i, j] = 2
-                else:
-                    grid[i, j] = 1
-            else:
-                grid[i, j] = 0
-    return grid
-
-
+@numba.jit(nopython=True, parallel=True)
 def extendGrid(grid):
     n = len(grid) + 2
     newGrid = np.zeros((n, n))
-    for i in range(n):
-        for j in range(n):
+    for i in numba.prange(n):
+        for j in numba.prange(n):
             if (i == 0 or j == 0) or (i == n - 1 or j == n - 1):
                 newGrid[i, j] = 3  # let 3 be the borders (later implement the reflected boundary)
             else:
@@ -30,11 +16,12 @@ def extendGrid(grid):
     return newGrid
 
 
-def Spread(gridEx, probImmune, probLightning):
+@numba.jit(nopython=True, parallel=True)
+def Spread(gridEx, probImmune, probLightning):  # Immune - 0.3 , probLightning - 0.001
     n = len(gridEx) - 2
     newGrid = np.zeros((n, n))
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
+    for i in numba.prange(1, n + 1):
+        for j in numba.prange(1, n + 1):
             isImmune = random.random()
             isLightning = random.random()
             # if Empty it will remain empty next time step
@@ -51,6 +38,7 @@ def Spread(gridEx, probImmune, probLightning):
     return newGrid
 
 
+@numba.jit(nopython=True, parallel=False)
 def MooreNeighborHood(grid, i, j):
     if grid[i - 1][j - 1] == 2 or grid[i + 1][j + 1] == 2 or grid[i + 1][j - 1] == 2 or grid[i - 1][j + 1] == 2 or \
             grid[i - 1][j] == 2 or grid[i][j + 1] == 2 or grid[i + 1][j] == 2 or grid[i][j - 1] == 2:
@@ -59,11 +47,12 @@ def MooreNeighborHood(grid, i, j):
         isTrue = False
     return isTrue
 
+
 def Simulation(time, forestGrid, probImmune=0.3, probLightning=0.001):
     grid = forestGrid
     gridList = [grid]
 
-    for i in range(time):
+    for i in numba.prange(time):
         ext = extendGrid(grid)
         grid = Spread(ext, probImmune, probLightning)  # Immune - 0.3 , probLightning - 0.001
         gridList.append(grid)
@@ -75,12 +64,14 @@ def Simulation(time, forestGrid, probImmune=0.3, probLightning=0.001):
     return gridList
 
 
+
+@numba.jit(nopython=True, parallel=True)
 def visualise(forestGrid):
     n = len(forestGrid)  # Maybe - 2?
     colorGrid = np.zeros((forestGrid.shape[0], forestGrid.shape[1], 3))
 
-    for i in range(0, n):
-        for j in range(0, n):
+    for i in numba.prange(0, n):
+        for j in numba.prange(0, n):
             if forestGrid[i, j] == 0:  # Empty Cell
                 colorGrid[i, j, 0] = 1
                 colorGrid[i, j, 1] = 1
