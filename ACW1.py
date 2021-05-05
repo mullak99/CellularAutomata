cdde0e76de34187b788386d816154ec688158b80
mullak99 <mullak99@mullak99.co.uk>
2021-05-05 18:00:24 +0100

    Final hopefully

--- ACW1.py
+++ ACW1.py
@@ -1,22 +1,38 @@
 import SpreadOfFire as simSeq
 import SpreadOfFireJit as simPar
-import multiprocessing as mp
-import cpuinfo
 import time
+import random
+
+def average(num):
+    sum_num = 0
+    for t in num:
+        sum_num = sum_num + t
+
+    avg = sum_num / len(num)
+    return avg
 
 
 if __name__ == '__main__':
-    iterations = 5
-    gridSize = 800
+    runMethodPerfCheck = False
+    perfTestSeed = True
+    runTimesEach = 1
+    runSeq = True
+    runPar = True
+
+    iterations = 10
+    gridSize = 2000
     probTree = 0.8
     probBurning = 0.01
     probImmune = 0.3
     probLighting = 0.001
 
-    print("= CPU Stats =")
-    print("CPU:", cpuinfo.get_cpu_info()["brand_raw"])
-    print("CPU Arch:", cpuinfo.get_cpu_info()["arch_string_raw"])
-    print("CPU threads:", mp.cpu_count())
+    if runMethodPerfCheck:
+        gridSize = 2000
+
+    if perfTestSeed:
+        seed = 675666552
+    else:
+        seed = random.randint(-2147483647, 2147483647)
 
     print("\n= Sim Stats =")
     print("Iterations:", iterations)
@@ -25,17 +41,96 @@ if __name__ == '__main__':
     print("Burning Probability:", probBurning)
     print("Immune Probability:", probImmune)
     print("Lightning Probability:", probLighting)
-
-    # Row Normalisation of a 2D array
-    fGrid = simSeq.forestGrid(probTree, probBurning, gridSize)
+    print("Forest Seed:", seed)
 
     print("\n= Performance Stats =")
-    # No parallelisation
-    startTime = time.time()
-    simSeq.Simulation(iterations, fGrid, probImmune, probLighting)
-    print("--- %s seconds - No Parallelisation---" % (time.time() - startTime))
 
-    # parallelisation - Numba
-    startTime = time.time()
-    simPar.Simulation(iterations, fGrid, probImmune, probLighting)
-    print("--- %s seconds - Parallelisation with Numba---" % (time.time() - startTime))
+    if runMethodPerfCheck:
+        seqRunTimesfg = []
+        seqRunTimeseg = []
+        seqRunTimessp = []
+        parRunTimesfg = []
+        parRunTimeseg = []
+        parRunTimessp = []
+
+        if runSeq:
+            for i in range(runTimesEach):
+                # No parallelisation
+                startTime = time.time()
+                fGridSeq = simSeq.forestGrid(probTree, probBurning, gridSize, seed)
+                timeRan = (time.time() - startTime)
+                seqRunTimesfg.append(timeRan)
+                print("--- %s seconds - No Parallelisation (forestGrid) ---" % timeRan)
+
+                startTime = time.time()
+                exGridSeq = simSeq.extendGrid(fGridSeq)
+                timeRan = (time.time() - startTime)
+                seqRunTimeseg.append(timeRan)
+                print("--- %s seconds - No Parallelisation (extendGrid) ---" % timeRan)
+
+                startTime = time.time()
+                simSeq.Spread(exGridSeq, probImmune, probLighting)
+                timeRan = (time.time() - startTime)
+                seqRunTimessp.append(timeRan)
+                print("--- %s seconds - No Parallelisation (Spread) ---" % timeRan)
+
+        if runPar:
+            for i in range(runTimesEach):
+                # parallelisation - Numba
+                startTime = time.time()
+                fGridPar = simPar.forestGrid(probTree, probBurning, gridSize, seed)
+                timeRan = (time.time() - startTime)
+                parRunTimesfg.append(timeRan)
+                print("--- %s seconds - Parallelisation with Numba (forestGrid) ---" % timeRan)
+
+                startTime = time.time()
+                exGridPar = simPar.extendGrid(fGridPar)
+                timeRan = (time.time() - startTime)
+                parRunTimeseg.append(timeRan)
+                print("--- %s seconds - Parallelisation with Numba (extendGrid) ---" % timeRan)
+
+                startTime = time.time()
+                simPar.Spread(exGridPar, probImmune, probLighting)
+                timeRan = (time.time() - startTime)
+                seqRunTimessp.append(timeRan)
+                print("--- %s seconds - Parallelisation with Numba (Spread) ---" % timeRan)
+
+        if runTimesEach > 1:
+            print("\n= Averages =")
+            if runSeq:
+                print("--- %s seconds - No Parallelisation (forestGrid) [Avg.] ---" % average(seqRunTimesfg))
+                print("--- %s seconds - No Parallelisation (extendGrid) [Avg.] ---" % average(seqRunTimeseg))
+                print("--- %s seconds - No Parallelisation (Spread) [Avg.] ---" % average(seqRunTimessp))
+            if runPar:
+                print("--- %s seconds - Parallelisation with Numba (forestGrid) [Avg.] ---" % average(parRunTimesfg))
+                print("--- %s seconds - Parallelisation with Numba (extendGrid) [Avg.] ---" % average(parRunTimeseg))
+                print("--- %s seconds - Parallelisation with Numba (Spread) [Avg.] ---" % average(parRunTimessp))
+
+    else:
+        seqRunTimes = []
+        parRunTimes = []
+
+        if runSeq:
+            for i in range(runTimesEach):
+                # No parallelisation
+                startTime = time.time()
+                simSeq.Simulation(iterations, gridSize, seed, probTree, probBurning, probImmune, probLighting)
+                timeRan = (time.time() - startTime)
+                seqRunTimes.append(timeRan)
+                print("--- %s seconds - No Parallelisation ---" % timeRan)
+
+        if runPar:
+            for i in range(runTimesEach):
+                # parallelisation - Numba
+                startTime = time.time()
+                simPar.Simulation(iterations, gridSize, seed, probTree, probBurning, probImmune, probLighting)
+                timeRan = (time.time() - startTime)
+                parRunTimes.append(timeRan)
+                print("--- %s seconds - Parallelisation with Numba ---" % timeRan)
+
+        if runTimesEach > 1:
+            print("\n= Averages =")
+            if runSeq:
+                print("--- %s seconds - No Parallelisation (Avg.) ---" % average(seqRunTimes))
+            if runPar:
+                print("--- %s seconds - Parallelisation with Numba (Avg.) ---" % average(parRunTimes))
--- SpreadOfFire.py
+++ SpreadOfFire.py
@@ -2,96 +2,102 @@ import numpy as np
 import random
 import cv2
 
-def forestGrid(probTree, probBurning, size):  # Prob Tree = 0.8 , Prob Burning = 0.01
+# Creates a new grid representing a forest, some trees start on-fire (probBurning)
+def forestGrid(probTree, probBurning, size, seed):
+    random.seed(seed)  # Allows forest generation to be the same, useful for performance checks
     grid = np.zeros((size, size))
-    for i in range(size):
-        for j in range(size):
-            isBurn = random.random()
+    for x in range(size):
+        for y in range(size):
             isTree = random.random()
-            if isTree < probTree:  # Been Switched from greater than
-                if isBurn < probBurning:  # Been Switched from less than
-                    grid[i, j] = 2
+            if isTree < probTree:  # isTree & probTree are 0.0-1.0. This cell will be a tree
+                isBurn = random.random()
+                if isBurn < probBurning:  # isBurn & probBurning are 0.0-1.0
+                    grid[x, y] = 2  # This cell is a burning tree
                 else:
-                    grid[i, j] = 1
+                    grid[x, y] = 1  # This cell is a normal tree
             else:
-                grid[i, j] = 0
+                grid[x, y] = 0
     return grid
 
-
+# Extends the current grid, adding a boundary (3) around all current cells
 def extendGrid(grid):
     n = len(grid) + 2
-    newGrid = np.zeros((n, n))
-    for i in range(n):
-        for j in range(n):
-            if (i == 0 or j == 0) or (i == n - 1 or j == n - 1):
-                newGrid[i, j] = 3  # let 3 be the borders (later implement the reflected boundary)
+    newGrid = np.zeros((n, n))  # New grid, extends boundary (+2)
+    for x in range(n):
+        for y in range(n):
+            if (x == 0 or y == 0) or (x == n - 1 or y == n - 1):
+                newGrid[x, y] = 3  # Boundary cells are represented by a 3
             else:
-                newGrid[i, j] = grid[i - 1, j - 1]
+                newGrid[x, y] = grid[x - 1, y - 1]
     return newGrid
 
-
+# Calculates fire spread of entire grid for one iteration
 def Spread(gridEx, probImmune, probLightning):
     n = len(gridEx) - 2
-    newGrid = np.zeros((n, n))
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            isImmune = random.random()
-            isLightning = random.random()
-            # if Empty it will remain empty next time step
-            if ((gridEx[i, j] == 2)):  # A burning cell becomes empty
-                newGrid[i - 1, j - 1] = 0
-            elif ((gridEx[i, j] == 1)):
-                if (isLightning < probLightning or MooreNeighborHood(gridEx, i, j)):  # or grid[i-1][j-1] == 2
-                    if (isImmune < probImmune):
-                        newGrid[i - 1, j - 1] = 1
+    newGrid = np.zeros((n, n))  # New grid, removes boundary (-2)
+    for x in range(1, n + 1):
+        for y in range(1, n + 1):
+            if gridEx[x, y] == 1:  # Current cell is a tree
+                isLightning = random.random()  # RNG for lightning strike
+                if isLightning < probLightning or MooreNeighbourhood(gridEx, x, y):
+                    isImmune = random.random()  # RNG for immunity
+                    if isImmune < probImmune:
+                        newGrid[x - 1, y - 1] = 1  # Tree is immune
                     else:
-                        newGrid[i - 1, j - 1] = 2
-            else:  # An empty cell remains empty
-                newGrid[i - 1, j - 1] = 0
+                        newGrid[x - 1, y - 1] = 2  # Tree is near another on-fire tree, or was hit by lightning
+                else:
+                    newGrid[x - 1, y - 1] = 1
+            else:  # Burning cells and other empty cells become/remain empty
+                newGrid[x - 1, y - 1] = 0
     return newGrid
 
 
-def MooreNeighborHood(grid, i, j):
-    if grid[i - 1][j - 1] == 2 or grid[i + 1][j + 1] == 2 or grid[i + 1][j - 1] == 2 or grid[i - 1][j + 1] == 2 or \
-            grid[i - 1][j] == 2 or grid[i][j + 1] == 2 or grid[i + 1][j] == 2 or grid[i][j - 1] == 2:
-        isTrue = True
-    else:
-        isTrue = False
-    return isTrue
+# Checks if any of the eight cells (inc. diagonal) around the current cell are on fire, if so, return true
+def MooreNeighbourhood(grid, x, y):
+    if grid[x - 1][y - 1] == 2 or grid[x + 1][y + 1] == 2 or grid[x + 1][y - 1] == 2 or grid[x - 1][y + 1] == 2 or \
+            grid[x - 1][y] == 2 or grid[x][y + 1] == 2 or grid[x + 1][y] == 2 or grid[x][y - 1] == 2:
+        return True
+    return False
 
-def Simulation(time, forestGrid, probImmune=0.3, probLightning=0.001):
-    grid = forestGrid
-    gridList = [grid]
+# Checks if any of the four cells (not inc. diagonal) around the current cell are on fire, if so, return true
+def VonNeumanNeighbourhood(grid, x, y):
+    if grid[x - 1][y - 1] == 2 or grid[x + 1][y + 1] == 2 or grid[x + 1][y - 1] == 2 or grid[x - 1][y + 1] == 2:
+        return True
+    return False
 
-    for i in range(time):
-        ext = extendGrid(grid)
-        grid = Spread(ext, probImmune, probLightning)  # Immune - 0.3 , probLightning - 0.001
-        gridList.append(grid)
-        v_grid = visualise(grid)
+# Runs the entire simulation for a preset time (no. of iterations)
+def Simulation(time, size, forestSeed, probTree=0.8, probBurning=0.01, probImmune=0.3, probLightning=0.001):
+    grid = forestGrid(probTree, probBurning, size, forestSeed)  # Creates the grid for the forest
+    gridList = [grid]  # Adds initial grid to a list/array
 
-        resized = cv2.resize(v_grid, (1000, 1000), interpolation=cv2.INTER_AREA)
-        cv2.imshow("Forest Fire Spread", resized)
+    for _ in range(time):  # Run simulation for preset number of iterations
+        ext = extendGrid(grid)  # Extend grid (add boundary)
+        grid = Spread(ext, probImmune, probLightning)  # Apply fire spread rules to the grid
+        gridList.append(grid)  # Adds grid to a list/array
+        v_grid = visualise(grid)  # Calculates the colours for the current grid
+
+        resized = cv2.resize(v_grid, (1000, 1000), interpolation=cv2.INTER_AREA)  # Visualise the current grid
+        cv2.imshow("Spread of Forest Fire", resized)
         cv2.waitKey(100)
     return gridList
 
 
+# Creates a visual representation of the forest grid, colouring the appropriate cells
 def visualise(forestGrid):
-    n = len(forestGrid)  # Maybe - 2?
+    n = len(forestGrid)
     colorGrid = np.zeros((forestGrid.shape[0], forestGrid.shape[1], 3))
-
-    for i in range(0, n):
-        for j in range(0, n):
-            if forestGrid[i, j] == 0:  # Empty Cell
-                colorGrid[i, j, 0] = 1
-                colorGrid[i, j, 1] = 1
-                colorGrid[i, j, 2] = 1
-            elif forestGrid[i, j] == 1:  # Blue - Tree
-                colorGrid[i, j, 0] = 1
-                colorGrid[i, j, 1] = 0
-                colorGrid[i, j, 2] = 0
-            elif forestGrid[i, j] == 2:  # Red - Burning Tree
-                colorGrid[i, j, 0] = 0
-                colorGrid[i, j, 1] = 0
-                colorGrid[i, j, 2] = 1
-
+    for x in range(0, n):
+        for y in range(0, n):
+            if forestGrid[x, y] == 0:  # 0 = Empty Cell
+                colorGrid[x, y, 0] = 1
+                colorGrid[x, y, 1] = 1
+                colorGrid[x, y, 2] = 1
+            elif forestGrid[x, y] == 1:  # 1 = Normal Tree (Green)
+                colorGrid[x, y, 0] = 0
+                colorGrid[x, y, 1] = 1
+                colorGrid[x, y, 2] = 0
+            elif forestGrid[x, y] == 2:  # 2 = Burning Tree (Red)
+                colorGrid[x, y, 0] = 0
+                colorGrid[x, y, 1] = 0
+                colorGrid[x, y, 2] = 1
     return colorGrid
